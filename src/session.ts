29b9e8513b8e415b22e7ee0cac9148594e51b5dc
Noah Cardoza <noahcardoza@gmail.com>
2020-08-09 01:27:02 -0700

    updated console.log to error

--- src/session.ts
+++ src/session.ts
@@ -125,7 +125,7 @@ export default {
           await sleep(5000)
           deleteFolderRecursive(userDataDirPath)
         } catch (e) {
-          console.log(e)
+          console.error(e)
           throw Error(`Error deleting browser session folder. ${e.message}`)
         }
       }
